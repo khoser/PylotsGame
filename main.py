4191bd2f6d6f713fd621509712ab37e67b0c78aa
ktu <ktuuuu@gmail.com>
2015-11-27 12:12:51 +0300

    anchorlayout вместо исходного widget

--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle, Color
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.graphics import Rectangle, Color #, Canvas
 
 
 class PG(Widget):
-  def __init__(self, d=10, *args, **kwargs):
-    Widget.__init__(self, *args, **kwargs)
-    self.d = d
+    def __init__(self, d=10, *args, **kwargs):
+        Widget.__init__(self, *args, **kwargs)
+        self.d = d
 
-  def on_touch_down(self, touch):
-    color = (random(), random(), random())
-    with self.canvas:
-      Color(*color)
-      Rectangle(pos=(self.center_x - self.d/2, self.center_y - self.d/2), size=(self.d,self.d))
-      #Rectangle(pos=(self.width-self.d/2, self.height-self.d/2), size=(self.d,self.d))
+    def on_touch_down(self, touch):
+        color = (random(), random(), random())
+        with self.canvas:
+            Color(*color)
+            Rectangle(pos=(self.center_x - self.d/2, self.center_y - self.d/2), size=(self.d,self.d))
+            #Rectangle(pos=(0, 0), size=(self.d,self.d))
+            #Rectangle(pos=(self.width-self.d/2, self.height-self.d/2), size=(self.d,self.d))
       
 
 class PylotsGameApp(App):
-  def build(self):
-    mwiget = Widget()
-    #mwiget.height
+    def build(self):
+        mwiget = AnchorLayout()
+        #btn = Button(text='Hello World')
+        #layout.add_widget(btn)
 
-    n1 = PG(200)
-    n2 = PG(100)
-    mwiget.add_widget(n1)
-    mwiget.add_widget(n2)
-    return mwiget
+        #mwiget = Widget()
+        #mwiget.height
+        #mwiget.canvas = Canvas(size = (300, 300), pos = (50, 50) )
+
+        n1 = PG(200)
+        n2 = PG(100)
+        mwiget.add_widget(n1)
+        mwiget.add_widget(n2)
+        return mwiget
 
 
 if __name__ == '__main__':
-  PylotsGameApp().run()
+    PylotsGameApp().run()
