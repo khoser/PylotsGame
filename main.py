f00457093f11a67675e3fb43d2049f93d221d5c3
ktu <ktuuuu@gmail.com>
2015-11-27 20:13:10 +0300

    field

--- main.py
+++ main.py
@@ -1,30 +1,68 @@
+# coding: utf-8
+
 from random import random
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.graphics import Rectangle, Color #, Canvas
-
+from PylotsGame_class import PylotMap_class
 
 class PG(Widget):
 #    def __init__(self, d=10, *args, **kwargs):
 #        Widget.__init__(self, *args, **kwargs)
 #        self.d = d
 
-    def fill(self):
-        pass
+    def fill(self,size,colortop,colorbottom,fieldSize):
+        self.PM = PylotMap_class(size,colortop,colorbottom)
+        self.fieldSize = fieldSize
+        self.squareSize = fieldSize / size - 1
+        self.redraw()
+
+    def redraw(self):
+        with self.canvas:
+            Color(1,1,1)
+            Rectangle(pos=(0, 0), size=(self.fieldSize,self.fieldSize))
+            for i in self.PM.matr:
+                for j in i:
+                    Color(*j.color)
+                    Rectangle(pos=(j.xplace * (self.squareSize + 1), j.yplace * (self.squareSize + 1)), size=(self.squareSize,self.squareSize))
+
+    def findxplace(self, x):
+        for i in self.PM.matr:
+            for j in i:
+                if j.xplace * (self.squareSize + 1) < x < j.xplace * (self.squareSize + 1) + self.squareSize:
+                    return j.xplace
+            break # одного прохода достаточно
+        return -1
+
+
+    def findyplace(self, y):
+        for i in self.PM.matr:
+            if i[0].yplace * (self.squareSize + 1) < y < i[0].yplace * (self.squareSize + 1) + self.squareSize:
+                    return i[0].yplace
+            break # одного прохода достаточно
+        return -1
+
 
     def on_touch_up(self, touch):
-        color = (random(), random(), random())
-        with self.canvas:
-            Color(*color)
-            #Rectangle(pos=(self.center_x - self.d/2, self.center_y - self.d/2), size=(self.d,self.d))
-            #Rectangle(pos=(0, 0), size=(self.d,self.d))
-            #Rectangle(pos=(self.width-self.d/2, self.height-self.d/2), size=(self.d,self.d))
-      
+        xplace = self.findxplace(touch.y)
+        yplace = self.findyplace(touch.x)
+        if (xplace >= 0) and (yplace >=0):
+            self.PM.invert(xplace,yplace)
+        self.redraw()
+
 
 class PylotsGameApp(App):
     def build(self):
-        mwiget = AnchorLayout(size=(300, 300))
+        fieldSize = 300
+        mwiget = AnchorLayout(size=(fieldSize, fieldSize))
+        colortop = (random(), random(), random())
+        colorbottom = (random(), random(), random())
+        game = PG()
+        game.fill(4,colortop,colorbottom,fieldSize)
+        mwiget.add_widget(game)
+
+
         #btn = Button(text='Hello World')
         #layout.add_widget(btn)
 
