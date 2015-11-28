3cc31e7819b73bb90a7b8c13c5bcb1d29e5db787
ktu <ktuuuu@gmail.com>
2015-11-28 11:03:23 +0300

    game and kv

--- main.py
+++ main.py
@@ -5,39 +5,53 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.button import Button
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.graphics import Rectangle, Color #, Canvas
 from PylotsGame_class import PylotMap_class
 
 class PG(Widget):
+
+    count = NumericProperty(0)
+    fieldSize = NumericProperty(300)
+
 #    def __init__(self, d=10, *args, **kwargs):
 #        Widget.__init__(self, *args, **kwargs)
 #        self.d = d
 
     def fill(self,size,colortop,colorbottom,fieldSize):
         self.PM = PylotMap_class(size,colortop,colorbottom)
-        self.fieldSize = fieldSize
+        self.fieldSize = fieldSize + 1
         self.squareSize = fieldSize / size - 1
+        self.x_clarif = 0
+        self.y_clarif = 0
+
+
+    def newgame(self):
+        self.PM.new()
+        self.count = self.PM.count
+
         self.redraw()
+
 
     def redraw(self):
         with self.canvas:
             Color(1,1,1)
-            Rectangle(pos=(0, 0), size=(self.fieldSize,self.fieldSize))
+            Rectangle(pos=(-self.x_clarif, -self.y_clarif), size=(self.fieldSize,self.fieldSize))
             for i in self.PM.matr:
                 for j in i:
                     Color(*j.color)
-                    Rectangle(pos=(j.xplace * (self.squareSize + 1), j.yplace * (self.squareSize + 1)), size=(self.squareSize,self.squareSize))
+                    Rectangle(pos=(1+j.xplace * (self.squareSize + 1)-self.x_clarif, 1+j.yplace * (self.squareSize + 1)-self.y_clarif), size=(self.squareSize,self.squareSize))
 
     def findxplace(self, x):
         for i in range(self.PM.size):
-            if self.PM.matr[i][i].xplace * (self.squareSize + 1) < x < self.PM.matr[i][i].xplace * (self.squareSize + 1) + self.squareSize:
+            if 1 + self.PM.matr[i][i].xplace * (self.squareSize + 1) - self.x_clarif < x < 1 + self.PM.matr[i][i].xplace * (self.squareSize + 1) + self.squareSize - self.x_clarif:
                 return self.PM.matr[i][i].xplace
         return -1
 
 
     def findyplace(self, y):
         for i in range(self.PM.size):
-            if self.PM.matr[i][i].yplace * (self.squareSize + 1) < y < self.PM.matr[i][i].yplace * (self.squareSize + 1) + self.squareSize:
+            if 1 + self.PM.matr[i][i].yplace * (self.squareSize + 1) - self.y_clarif < y < 1 + self.PM.matr[i][i].yplace * (self.squareSize + 1) + self.squareSize - self.y_clarif:
                 return self.PM.matr[i][i].yplace
         return -1
 
@@ -47,6 +61,7 @@ class PG(Widget):
         yplace = self.findyplace(touch.x)
         if (xplace >= 0) and (yplace >=0):
             self.PM.invert(xplace,yplace)
+            self.count = self.PM.count
         self.redraw()
         if self.PM.congratulation():
             self.makesometext()
@@ -55,35 +70,34 @@ class PG(Widget):
         print("You won!")
 
 
-class PylotsGameApp(App):
-    def build(self):
-        fieldSize = 300
-        mwiget = AnchorLayout(anchor_x='center', anchor_y='top')
-        menu = AnchorLayout(anchor_x='left', anchor_y='top')
-        gm = AnchorLayout(anchor_x='center', anchor_y='center')
+    def onbuttonpress(self):
+
         colortop = (random(), random(), random())
         colorbottom = (random(), random(), random())
-        game = PG()
-        game.fill(4,colortop,colorbottom,fieldSize)
-        gm.add_widget(game)
 
-        btn = Button(text='New')
-        menu.add_widget(btn)
+        fieldSize = 300
+        fieldnum = 4
 
-        mwiget.add_widget(menu)
-        mwiget.add_widget(gm)
+        self.fill(fieldnum,colortop,colorbottom,fieldSize)
+        self.newgame()
 
 
+class GameDesk(Widget):
+    pg = ObjectProperty(None)
 
-        #mwiget = Widget()
-        #mwiget.height
-        #mwiget.canvas = Canvas(size = (300, 300), pos = (50, 50) )
+    def onbuttonpress(self):
+        self.pg.onbuttonpress()
 
-        #n1 = PG(200)
-        #n2 = PG(100)
-        #mwiget.add_widget(n1)
-        #mwiget.add_widget(n2)
-        return mwiget
+
+class PylotsGameApp(App):
+    def build(self):
+
+        game = GameDesk()
+
+        game.onbuttonpress()
+
+
+        return game
 
 
 if __name__ == '__main__':
